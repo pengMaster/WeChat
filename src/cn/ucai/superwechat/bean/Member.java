4b7024549c7570159a2e4cd9e23931422c467311
wp521 <946549990@qq.com>
2016-05-19 16:27:12 -0700

    在Member中修改equals方法

--- src/cn/ucai/superwechat/bean/Member.java
+++ src/cn/ucai/superwechat/bean/Member.java
@@ -100,12 +100,10 @@ public class Member extends User implements java.io.Serializable {
 		if (!(o instanceof Member)) return false;
 
 		Member member = (Member) o;
-
-		if (!mmemberUserId.equals(member.mmemberUserId)) return false;
-		if (!mmemberUserName.equals(member.mmemberUserName)) return false;
-		if (!mmemberGroupId.equals(member.mmemberGroupId)) return false;
-		return mmemberGroupHxid.equals(member.mmemberGroupHxid);
-
+		return  (mmemberUserId.equals(member.mmemberUserId)
+				&&mmemberUserName.equals(member.mmemberUserName)
+				&&mmemberGroupId.equals(member.mmemberGroupId)
+				&&mmemberGroupHxid.equals(member.mmemberGroupHxid));
 	}
 
 	@Override
