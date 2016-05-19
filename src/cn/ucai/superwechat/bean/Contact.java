05c248fb70542b81d0f8808fbb25e3dca785e3bd
wp521 <946549990@qq.com>
2016-05-19 15:17:44 -0700

    在Contact中添加equals和hashcode方法

--- src/cn/ucai/superwechat/bean/Contact.java
+++ src/cn/ucai/superwechat/bean/Contact.java
@@ -15,6 +15,23 @@ public class Contact extends User implements java.io.Serializable {
 	private Integer mcontactUserId;
 	private String mcontactUserName;
 	private Integer mcontactCid;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Contact)) return false;
+
+		Contact contact = (Contact) o;
+
+		return mcontactId.equals(contact.mcontactId);
+
+	}
+
+	@Override
+	public int hashCode() {
+		return mcontactId.hashCode();
+	}
+
 	private String mcontactCname;
 
 	// Constructors
