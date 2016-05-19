c00bbb643386b6682ae2a832bd1d9cb5ede7f9cd
wp521 <946549990@qq.com>
2016-05-19 11:47:26 -0700

    在SuperWeChatApplication定义SERVER_ROOT服务器地址

--- src/cn/ucai/superwechat/SuperWeChatApplication.java
+++ src/cn/ucai/superwechat/SuperWeChatApplication.java
@@ -19,6 +19,7 @@ import android.content.Context;
 import com.easemob.EMCallBack;
 
 public class SuperWeChatApplication extends Application {
+	public  static String SERVER_ROOT = "http://10.0.2.2:8080/SuperWeChatServer/Server";
 
 	public static Context applicationContext;
 	private static SuperWeChatApplication instance;
@@ -84,7 +85,7 @@ public class SuperWeChatApplication extends Application {
 	/**
 	 * 设置用户名
 	 *
-	 * @param user
+	 * @param username
 	 */
 	public void setUserName(String username) {
 	    hxSDKHelper.setHXId(username);
